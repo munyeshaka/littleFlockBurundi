7dd7613de78d6cf729f3b833320d00c8affdaee4
munyeshaka <aimablemunyeshaka30@gmail.com>
2021-12-10 15:42:34 +0200

    convert php link in django of menu

--- flock/urls.py
+++ flock/urls.py
@@ -8,7 +8,13 @@ app_name = 'flock'
 
 urlpatterns = [
     re_path(r'^$', views.home, name='home'),
+    re_path(r'programs/', views.sermon_list, name='programs'),
     re_path(r'sermons/', views.sermon_list, name='sermons'),
+    re_path(r'articles/', views.sermon_list, name='articles'),
+    re_path(r'videos/', views.sermon_list, name='videos'),
+    re_path(r'events/', views.sermon_list, name='events'),
+    re_path(r'support_us/', views.sermon_list, name='support_us'),
+    re_path(r'contact/', views.sermon_list, name='contact'),
     re_path(r'^(?P<slug>[\w-]+)/$', views.sermon_detail, name='sermon_detail'),
 ]
 
