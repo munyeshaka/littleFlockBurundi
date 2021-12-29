bdc49e9e251e7bf0160fbc8a6ef9036df4357f0d
munyeshaka <aimablemunyeshaka30@gmail.com>
2021-12-29 10:47:50 +0200

    events

--- flock_kirundi/forms.py
+++ flock_kirundi/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class ContactMeForm(forms.Form):
-    emailid = forms.EmailField(widget=forms.TextInput(attrs={'class':'form-control form-control-lg fs-6 border-0 shadow-sm', 'placeholder':'Your email'}), required=True)
-    subject = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control form-control-lg fs-6 border-0 shadow-sm', 'placeholder':'Subject'}), required=True)
-    message = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control form-control-lg fs-6 border-0 shadow-sm', 'placeholder':'Your Message'}), required=True)
+    emailid = forms.EmailField(widget=forms.TextInput(attrs={'class':'form-control form-control-lg fs-6 border-0 shadow-sm', 'placeholder':'email yawe'}), required=True)
+    subject = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control form-control-lg fs-6 border-0 shadow-sm', 'placeholder':'igituma '}), required=True)
+    message = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control form-control-lg fs-6 border-0 shadow-sm', 'placeholder':'Twandikire nyaha'}), required=True)
