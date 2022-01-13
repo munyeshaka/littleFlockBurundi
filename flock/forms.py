eef3ba65b7088190060ca048a84ce9e94e305967
munyeshaka <aimablemunyeshaka30@gmail.com>
2022-01-13 12:11:10 +0200

    contact us design debug

--- flock/forms.py
+++ flock/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class ContactMeForm(forms.Form):
-    emailid = forms.EmailField(widget=forms.TextInput(attrs={'class':'form-control form-control-lg fs-6 border-0 shadow-sm', 'placeholder':'Your email'}), required=True)
-    subject = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control form-control-lg fs-6 border-0 shadow-sm', 'placeholder':'Subject'}), required=True)
-    message = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control form-control-lg fs-6 border-0 shadow-sm', 'placeholder':'Your Message'}), required=True)
+    emailid = forms.EmailField(widget=forms.TextInput(attrs={'class':'form-control form-control-lg fs-6 border-0 shadow-sm', 'style':'background-color:#f6f6f8', 'placeholder':'Your email', 'required':'true'}), required=True)
+    subject = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control form-control-lg fs-6 border-0 shadow-sm', 'style':'background-color:#f6f6f8','placeholder':'Subject', 'required':'true'}), required=True)
+    message = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control form-control-lg fs-6 border-0 shadow-sm', 'style':'background-color:#f6f6f8', 'placeholder':'Your Message', 'required':'true'}), required=True)
