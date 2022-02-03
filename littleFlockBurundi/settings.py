077478bdd3f3817fbbb3004c9374f37fb927e485
Munyeshaka <aimablemunyeshaka30@gmail.com>
2022-02-03 09:14:29 -0800

    debug

--- littleFlockBurundi/settings.py
+++ littleFlockBurundi/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms',
-    'sorl.thumbnail',
+    #'crispy_forms',
+    #'sorl.thumbnail',
     'flock',
     'flock_kirundi',
 
